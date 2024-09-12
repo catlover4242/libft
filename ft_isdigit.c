32485e2dfb14454c04ea8e9a48efc78de17c0b45
paul <akapaui@protonmail.com>
2024-09-12 16:51:42 +0200

    added ft_isdigit.c

---
+++ ft_isdigit.c
@@ -0,0 +1,23 @@
+#include "libft.h"
+#include <stdio.h>
+#include <ctype.h>
+
+int     ft_isdigit(int c)
+{
+    if (c >= 48 && c <= 57)
+    {
+        return (1);
+    }
+    else
+        return (0);
+}
+
+/*
+int main()
+{
+    char c = '5';
+    printf("ft_isdigit: %d\n", ft_isdigit(c));
+    printf("isdigit:    %d\n", isdigit(c));
+    return 0;
+}
+*/
