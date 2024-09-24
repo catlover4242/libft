9ce2fa93de72545943a3b2e79fb9bcd687965a03
paul <akapaui@protonmail.com>
2024-09-24 22:56:08 +0200

    added ft_itoa.c

---
+++ ft_itoa.c
@@ -0,0 +1,48 @@
+#include "libft.h"
+
+static int  compteur(long n)
+{
+    int count;
+    
+    count = 1;
+    if (n < 0)
+    {
+        n = -n;
+        count++;
+    }
+    while (n >= 10)
+    {
+        n /= 10;
+        count++;
+    }
+    return (count);
+}
+
+char    *ft_itoa(int n)
+{
+    char    *o;
+    long    n1;
+    int     nb;
+    int     sign;
+
+    n1 = n;
+    sign = 0;
+    if (n < 0)
+    {
+        sign = 1;
+        n1 = -n1;
+    }
+    nb = compteur(n1);
+    o = malloc(nb + 1);
+    if (!o)
+        return (NULL);
+    o[nb] = '\0';
+    while (nb > 0)
+    {
+        o[--nb] = (n1 % 10) + '0';
+        n1 /= 10;
+    }
+    if (sign)
+        o[0] = '-';
+    return (o);
+}
