1c3176a840c66671090a699c9784299928aca9f5
paul <akapaui@protonmail.com>
2024-09-25 18:01:04 +0200

    strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,52 @@
+#include "libft.h"
+
+static int  prout(char const *s1, char const *set)
+{
+    size_t      i;
+    size_t      len;
+
+    i = 0;
+    len = strlen(s1);
+    while (i < len && ft_strchr(set, s1[i] != NULL))
+    {
+        i++;
+    }
+    return (i);
+}
+
+static int  prouuut(char const *s1, char const *set)
+{
+    size_t      i;
+    size_t      len;
+
+    i = 0;
+    len = strlen(s1);
+    while (i < len && ft_strchr(set, s1[len - i - 1]) != NULL)
+    {
+        i++;
+    }
+    return (len - i);
+}
+
+char *ft_strtrim(char const *s1, char const *set)
+{
+    int     debut;
+    int     fin;
+    char    *str;
+
+    if (s1 == NULL || set == NULL)
+        return (NULL);
+    
+    debut = prout(s1, set);
+    fin = prouuut(s1, set);
+
+    if (fin <= debut)
+        return (ft_strdup(""));
+
+    str = (char *)malloc(sizeof(char) * (fin - debut + 1));
+    if (str == NULL)
+        return (NULL);
+    ft_strlcpy(str, s1 + debut, fin - debut + 1);
+    free(s1);
+    return (str);
+}
